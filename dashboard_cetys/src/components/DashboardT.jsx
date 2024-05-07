36339d024c3cabc9c0d76234af95a40062585f5c
SantiagoCamm <camarenscode@gmail.com>
2024-05-07 11:05:55 -0700

    Cree la pantalla principal del proyecto, en Login

--- dashboard_cetys/src/components/DashboardT.jsx
+++ dashboard_cetys/src/components/DashboardT.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function DashboardT() {
   return (
-    <div>DashboardT</div>
+    <div></div>
   )
 }
 
--- dashboard_cetys/src/components/LoginForm.jsx
+++ dashboard_cetys/src/components/LoginForm.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import './LoginForm.css';
 
-const LoginForm = () =>{
-    return(
-        <div>Login Form</div>
-    );
+const LoginForm = () => {
+  return (
+    <div className="wrapper">
+      <form>
+        <h1>Login</h1>
+        <div className="BAdmin">
+          <a href="/pag1"> {/* Utiliza <a> con href */}
+            <button>Administrador</button>
+          </a>
+        </div>
+        <div className="BTrabajador">
+          <a href="/pag2"> {/* Utiliza <a> con href */}
+            <button>Trabajador</button>
+          </a>
+        </div>
+        <div className="BUsuario">
+          <a href="/pag3">
+            <button>Usuario</button>
+          </a>
+        </div>
+      </form>
+      <img className="foto" src="https://conadeipfba.org.mx/sites/default/files/dev1/sites/default/files/images/equipos/logos/zorros-mxl_1.png" alt="Zorros Logo" />
+    </div>
+  );
 };
 
 export default LoginForm;
