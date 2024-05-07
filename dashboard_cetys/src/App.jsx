1c47f4f5eb25ff9dfa6d9d285eb6913df8ca4cdb
SantiagoCamm <camarenscode@gmail.com>
2024-05-06 23:04:37 -0700

    Esqueleto

--- dashboard_cetys/src/App.jsx
+++ dashboard_cetys/src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import DashboardT from './components/DashboardT'
+import LoginForm from './components/LoginForm'
 
 function App() {
   return (
     <div>
       <DashboardT/>
-      
-      <div>tavo</div>
+      <div>
+        <LoginForm/>
+      </div>
     </div>
    
-
-
     /* Esta es la pagina de santiago, aqui vas a hacer el login que nos va a llevar a los componentes */ 
   )
 }
